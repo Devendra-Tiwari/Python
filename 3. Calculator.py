5f6c46873d4a7c2a35b56a1f7b0c953b0e4429ba
Devendra-Tiwari <Devendra97953@gmail.com>
2024-10-26 15:34:49 +0530

    Create 3. Calculator.py

---
+++ 3. Calculator.py
@@ -0,0 +1,13 @@
+'''
+Create a calculator capable of performing addition, subtraction,
+multiplication and division operations on two numbers.
+Your program should format the output in a readable manner!
+'''
+
+a = int(input("Enter number A: "))
+b = int(input("Enter number B: "))
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"x",b,"=",a*b)
+print(a,"/",b,"=",a/b)
